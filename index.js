61bf1af9323d07482c7c32a8ab3bb0e3d8c10fb1
Sharon Rumsey <sharon@rumsey.nz>
2016-02-28 20:38:50 +1300

    colt review of express

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 var express = require('express');
 var app = express();
+// var ejs = require('ejs'),
 
+// couldn't get this page to work
+// app.get('/', function(req, res) {
+// 	res.render('dogs.ejs')
+// })
+
+// order of routes matter
+// '/' => hi there
 app.get('/', function(req, res) {
-	res.send('Hello world!  hello')
+	res.send('Hello world!')
+})
+// '/goodbye' => goodbye
+app.get('/goodbye', function(req, res) {
+	res.send('Goodbye')
+})
+// '/cat' => cat
+app.get('/cat', function(req, res) {
+	res.send('cat!')
 })
 
-app.get('/yo', function(req, res) {
-	res.send('YO!')
+app.get('/fallinlovewith/:thing', function(req, res) {
+	var thing = req.params.thing;
+	res.send("You fell in Love with " + thing)
 })
 
+
+app.get("*", function(req, res) {
+	res.send("You are lost")
+})
+
+// telling express to listen
 var server = app.listen(3000, function() {
 	console.log('Server running at http://localhost: ', + server.address().port)
 })
